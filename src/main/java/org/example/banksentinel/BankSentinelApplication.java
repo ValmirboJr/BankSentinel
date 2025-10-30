6bbdc7a8818cfc1446e120b977a7ba99972f87bb
ValmirboJr <valmirborgesojr@gmail.com>
2025-10-30 12:49:39 -0300

    Feat:Inicio do Projeto

---
+++ src/main/java/org/example/banksentinel/BankSentinelApplication.java
@@ -0,0 +1,13 @@
+package org.example.banksentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSentinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/banksentinel/BankSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.banksentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
