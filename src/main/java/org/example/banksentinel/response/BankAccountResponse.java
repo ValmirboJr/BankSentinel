9225da34ba89e7c4255e7e5ad58a8df9249c1b5e
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-02 23:46:44 -0300

    Feat:Created Response to represent account data returned by the API

--- src/main/java/org/example/banksentinel/response/BankAccountResponse.java
+++ src/main/java/org/example/banksentinel/response/BankAccountResponse.java
@@ -1,4 +1,13 @@
 package org.example.banksentinel.response;
 
-public class BankAccountResponse {
+import lombok.Builder;
+import org.example.banksentinel.enums.Banks;
+import org.example.banksentinel.enums.CurrencyType;
+import org.example.banksentinel.enums.TypeAccount;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record BankAccountResponse(Long id, Banks banks, String accountNumber, TypeAccount typeAccount, CurrencyType currency, double AccountBalance,
+                                  LocalDateTime registrationDate) {
 }
