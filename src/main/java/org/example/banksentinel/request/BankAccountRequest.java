0367d2ef5be8006ca50cc227527fcaf11e900b84
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-02 23:45:35 -0300

    Feat:Created Request DTO for account creation and updates

--- src/main/java/org/example/banksentinel/request/BankAccountRequest.java
+++ src/main/java/org/example/banksentinel/request/BankAccountRequest.java
@@ -1,4 +1,13 @@
 package org.example.banksentinel.request;
 
-public record BankAccountRequest() {
+import lombok.Builder;
+import org.example.banksentinel.enums.Banks;
+import org.example.banksentinel.enums.CurrencyType;
+import org.example.banksentinel.enums.TypeAccount;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record BankAccountRequest(Banks bank, String accountNumber, TypeAccount typeAccount, CurrencyType currency, double AccountBalance,
+                                 LocalDateTime registrationDate) {
 }
