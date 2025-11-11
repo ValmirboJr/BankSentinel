4cd1a85aad4b70f57d0406370facfeb43ebc5eee
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:23:31 -0300

    Feat:Created TransactionRequest for requiredArgs for create object

--- src/main/java/org/example/banksentinel/request/TransactionRequest.java
+++ src/main/java/org/example/banksentinel/request/TransactionRequest.java
@@ -1,4 +1,15 @@
 package org.example.banksentinel.request;
 
-public class TransactionRequest {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.example.banksentinel.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+public record TransactionRequest(Long bank_account_id, @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")LocalDateTime eventTime, BigDecimal amount,
+                                 @JsonInclude PaymentMethod paymentMethod,String description, Integer mcc,String category)
+{
 }
