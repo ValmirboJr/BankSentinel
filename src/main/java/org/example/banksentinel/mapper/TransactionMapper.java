478c776d1e3b905ecb6094fe0c0e680a4df2ca8b
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:18:53 -0300

    Feat: implement TransactionMapper for entity-DTO transformation with enums support

--- src/main/java/org/example/banksentinel/mapper/TransactionMapper.java
+++ src/main/java/org/example/banksentinel/mapper/TransactionMapper.java
@@ -1,4 +1,42 @@
 package org.example.banksentinel.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.banksentinel.entity.BankAccount;
+import org.example.banksentinel.entity.Transaction;
+import org.example.banksentinel.request.TransactionRequest;
+import org.example.banksentinel.response.TransactionResponse;
+
+@UtilityClass
 public class TransactionMapper {
+
+    public static Transaction toTransaction(TransactionRequest transactionRequest){
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setId(bankAccount.getId());
+
+        return Transaction
+                .builder()
+                .eventTime(transactionRequest.eventTime())
+                .amount(transactionRequest.amount())
+                .paymentMethod(transactionRequest.paymentMethod())
+                .description(transactionRequest.description())
+                .mcc(transactionRequest.mcc())
+                .category(transactionRequest.category())
+                .build();
+    }
+
+    public static TransactionResponse toTransactionResponse(Transaction transaction){
+
+            return TransactionResponse
+                    .builder()
+                    .id(transaction.getId())
+                    .bank_account_id(transaction.getAccount().getId())
+                    .eventTime(transaction.getEventTime())
+                    .amount(transaction.getAmount())
+                    .paymentMethod(transaction.getPaymentMethod())
+                    .description(transaction.getDescription())
+                    .mcc(transaction.getMcc())
+                    .category(transaction.getCategory())
+                    .build();
+        }
 }
