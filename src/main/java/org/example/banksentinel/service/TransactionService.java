5ae05f8cdbcde0e8b4bcbdded93bb69f960ced0c
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:29:12 -0300

    feat: Create CRUD methods for transaction

--- src/main/java/org/example/banksentinel/service/TransactionService.java
+++ src/main/java/org/example/banksentinel/service/TransactionService.java
@@ -1,4 +1,45 @@
 package org.example.banksentinel.service;
 
+import org.example.banksentinel.entity.BankAccount;
+import org.example.banksentinel.entity.Transaction;
+import org.example.banksentinel.mapper.TransactionMapper;
+import org.example.banksentinel.repository.BankAccountRepository;
+import org.example.banksentinel.repository.TransactionRepository;
+import org.example.banksentinel.request.TransactionRequest;
+import org.example.banksentinel.response.TransactionResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+
+    public TransactionResponse createTrn(TransactionRequest request) {
+        BankAccount account = bankAccountRepository.findById(request.bank_account_id())
+                .orElseThrow(() -> new RuntimeException("Conta bancária não encontrada"));
+
+        Transaction transaction = TransactionMapper.toTransaction(request);
+        transaction.setAccount(account);
+        transaction = transactionRepository.save(transaction);
+
+        return TransactionMapper.toTransactionResponse(transaction);
+    }
+
+    public Optional<Transaction> getTrn(Long id) {
+        return transactionRepository.findById(id);
+    }
+    public List<Transaction> getAllTrn() {
+        return transactionRepository.findAll();
+    }
+    public void DeleteTrn(Long id) {
+        transactionRepository.deleteById(id);
+    }
 }
