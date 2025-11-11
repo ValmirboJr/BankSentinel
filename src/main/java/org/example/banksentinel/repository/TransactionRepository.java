4ae3a66f48d2170c2456507ef4b88e0a8d505e6a
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:18:58 -0300

    Refactor: update TransactionRepository to align with enum-based entity fields

--- src/main/java/org/example/banksentinel/repository/TransactionRepository.java
+++ src/main/java/org/example/banksentinel/repository/TransactionRepository.java
@@ -1,4 +1,7 @@
 package org.example.banksentinel.repository;
 
-public class TransactionRepository {
+import org.example.banksentinel.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
