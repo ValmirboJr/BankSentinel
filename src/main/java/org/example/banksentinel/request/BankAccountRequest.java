b76a5031f0d330509495e207bc1b33d5cf479350
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:21:55 -0300

    feat: update BankAccountRequest to include relationship with transation

--- src/main/java/org/example/banksentinel/request/BankAccountRequest.java
+++ src/main/java/org/example/banksentinel/request/BankAccountRequest.java
@@ -1,13 +1,16 @@
 package org.example.banksentinel.request;
 
 import lombok.Builder;
+import org.example.banksentinel.entity.Transaction;
 import org.example.banksentinel.enums.Banks;
 import org.example.banksentinel.enums.CurrencyType;
 import org.example.banksentinel.enums.TypeAccount;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
-public record BankAccountRequest(Banks bank, String accountNumber, TypeAccount typeAccount, CurrencyType currency, double AccountBalance,
-                                 LocalDateTime registrationDate) {
+public record BankAccountRequest(Banks bank, String accountNumber, TypeAccount typeAccount, CurrencyType currency, BigDecimal accountBalance,
+                                 LocalDateTime registrationDate, List<Transaction> transactions) {
 }
