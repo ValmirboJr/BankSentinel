abced92b27ebbfe1e51049b1c569cd1ae8b77997
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 07:54:49 -0300

    Feat:Created Transaction entity and add relationship with BankAccount

--- src/main/java/org/example/banksentinel/entity/Transaction.java
+++ src/main/java/org/example/banksentinel/entity/Transaction.java
@@ -1,4 +1,45 @@
 package org.example.banksentinel.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.banksentinel.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transaction")
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "bank_account_id", nullable = false, foreignKey = @ForeignKey(name = "fk_txn_account"))
+    @JsonBackReference
+    private BankAccount account;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime eventTime;
+
+    private BigDecimal amount;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private String description;
+
+    private Integer mcc;
+
+    private String category;
 }
