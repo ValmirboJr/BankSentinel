4149ac4c060014dbcd9c9221b3ef6f5fc54fb9d3
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:18:27 -0300

    Feat: implement BankAccountMapper for entity-DTO transformation

--- src/main/java/org/example/banksentinel/mapper/BankAccountMapper.java
+++ src/main/java/org/example/banksentinel/mapper/BankAccountMapper.java
@@ -2,24 +2,40 @@ package org.example.banksentinel.mapper;
 
 import lombok.experimental.UtilityClass;
 import org.example.banksentinel.entity.BankAccount;
+import org.example.banksentinel.entity.Transaction;
 import org.example.banksentinel.request.BankAccountRequest;
 import org.example.banksentinel.response.BankAccountResponse;
+import org.example.banksentinel.response.TransactionResponse;
+
+import java.util.List;
 
 @UtilityClass
 public class BankAccountMapper {
 
     public static BankAccount toBankAccount(BankAccountRequest bankAccountRequest) {
+
+        List<Transaction> transactions = bankAccountRequest.transactions().stream()
+                .map(transactionid -> Transaction.builder().id(transactionid.getId()).build())
+                .toList();
+
         return BankAccount
                 .builder()
                 .banks(bankAccountRequest.bank())
                 .accountNumber(bankAccountRequest.accountNumber())
                 .typeAccount(bankAccountRequest.typeAccount())
                 .currency(bankAccountRequest.currency())
-                .AccountBalance(bankAccountRequest.AccountBalance())
+                .accountBalance(bankAccountRequest.accountBalance())
                 .registrationDate(bankAccountRequest.registrationDate())
+                .transactions(transactions)
                 .build();
     }
     public static BankAccountResponse toBankAccountResponse(BankAccount bankAccount) {
+
+        List<TransactionResponse> transactions = bankAccount.getTransactions()
+                .stream()
+                .map(TransactionMapper::toTransactionResponse)
+                .toList();
+
         return BankAccountResponse
                 .builder()
                 .id(bankAccount.getId())
@@ -27,8 +43,9 @@ public class BankAccountMapper {
                 .accountNumber(bankAccount.getAccountNumber())
                 .typeAccount(bankAccount.getTypeAccount())
                 .currency(bankAccount.getCurrency())
-                .AccountBalance(bankAccount.getAccountBalance())
+                .accountBalance(bankAccount.getAccountBalance())
                 .registrationDate(bankAccount.getRegistrationDate())
+                .transactions(transactions)
                 .build();
     }
 }
