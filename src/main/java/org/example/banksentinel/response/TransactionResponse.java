8978a4f8c1f0ab99e3ef195c9014a2ca1e775e85
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-11 09:27:57 -0300

    Feat:Add BankAccount relationship

--- src/main/java/org/example/banksentinel/response/TransactionResponse.java
+++ src/main/java/org/example/banksentinel/response/TransactionResponse.java
@@ -1,4 +1,14 @@
 package org.example.banksentinel.response;
 
-public record TransactionResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.example.banksentinel.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+public record TransactionResponse(Long id, Long bank_account_id, @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime eventTime, BigDecimal amount,
+                                  @JsonInclude PaymentMethod paymentMethod, String description, Integer mcc, String category) {
 }
