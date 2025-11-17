392cb37938b6c784ffa1117a21b3b93ec0dde15d
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-17 08:17:34 -0300

    feat:add email colunm

--- src/main/resources/db/migration/V3__add_email.sql
+++ src/main/resources/db/migration/V3__add_email.sql
@@ -1,6 +1,2 @@
 ALTER TABLE bank_account
-    ADD COLUMN customer_phone VARCHAR(20);
-
-UPDATE bank_account
-SET customer_phone = '+5511999999999'
-WHERE customer_phone IS NULL;
+    ADD COLUMN email VARCHAR(255);
