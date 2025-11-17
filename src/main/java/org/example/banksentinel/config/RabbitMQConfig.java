dd9f664543bf0458203f9f0635987a6c91302080
ValmirboJr <valmirborgesojr@gmail.com>
2025-11-17 08:25:17 -0300

    feat:add RabbitMQ config for email messaging

--- src/main/java/org/example/banksentinel/config/RabbitMQConfig.java
+++ src/main/java/org/example/banksentinel/config/RabbitMQConfig.java
@@ -1,4 +1,50 @@
 package org.example.banksentinel.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+
+    public static final String TRANSACTION_QUEUE = "transaction.queue";
+    public static final String TRANSACTION_EXCHANGE = "transaction.exchange";
+    public static final String TRANSACTION_ROUTING_KEY = "transaction.created";
+
+
+    @Bean
+    public Queue transactionQueue() {
+        return new Queue(TRANSACTION_QUEUE, true);
+    }
+
+    @Bean
+    public DirectExchange transactionExchange() {
+        return new DirectExchange(TRANSACTION_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue transactionQueue, DirectExchange transactionExchange) {
+        return BindingBuilder
+                .bind(transactionQueue)
+                .to(transactionExchange)
+                .with(TRANSACTION_ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
 }
